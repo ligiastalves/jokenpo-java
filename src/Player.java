3cba1041a8ef64dbd584353374def04dc9fbec47
Ligia Alves <ligia.stalves@gmail.com>
2022-12-07 09:29:28 -0300

    update

--- src/Player.java
+++ src/Player.java
@@ -7,24 +7,14 @@ public class Player {
         this.name = nome;
         this.score = 0;
     }
-
-    public void incrementScore(){
-        setScore(getScore() +1);
+    public void incrementScore() {
+        this.score += 1;
     }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getScore() {
         return score;
-    }
-
-    public void setScore(Integer score) {
-        this.score = score;
     }
 }
